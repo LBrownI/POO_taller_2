f81102755dced9e9d422035d5493d9931f8dc6a0
Alan <129406592+AlanSilvaaa@users.noreply.github.com>
2023-05-24 21:30:42 -0400

    Add files via upload

--- encoder-decoder/src/test.java
+++ encoder-decoder/src/test.java
@@ -2,10 +2,12 @@ public class test {
     public static void main(String[] args){
 
 
-        int[] C = new int[3];
-        C[1] = 12;
-        int a = 13;
-        String b = Integer.toBinaryString(a);
-        System.out.println(b);
+        int a = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                a++;
+            }
+        }
+        System.out.println(a);
     }
 }
