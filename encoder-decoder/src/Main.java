5801a8bcd42ff8c70de434a3f0007f3c0597c319
Alan <129406592+AlanSilvaaa@users.noreply.github.com>
2023-05-25 09:58:36 -0400

    Add files via upload

--- encoder-decoder/src/Main.java
+++ encoder-decoder/src/Main.java
@@ -16,6 +16,25 @@ public class Main {
         int width = image.getWidth();
         int height = image.getHeight();
 
+        int letterBytesLenght = 0;
+        int letterBytePosition = 0;
+        int charPosition = 0;
+
+        //convierte el input del usuario a un array de bytes por cada caracter
+        String inputWord = "holas#";
+        byte[] bytes = inputWord.getBytes();
+
+        System.out.println("los bites son: " +bytes[0]);
+
+        String[] inputWordBytesArray = new String[inputWord.length()];
+        int wordToByteIterator = 0;
+        for (int iterator : bytes) {
+            inputWordBytesArray[wordToByteIterator] = Integer.toBinaryString(iterator);
+            wordToByteIterator++;
+        }
+
+        System.out.println("la letra h en binario es: " +inputWordBytesArray[0]);
+
         for (int y = 0; y < 1; y++) {           //CHANGE THE "1" OF THE FIRST FOR TO HEIGHT LATER!!!1!!11!!1!!!!
             for (int x = 0; x < width; x++) {
                 int pixel = image.getRGB(x, y);
@@ -30,35 +49,19 @@ public class Main {
                 int binaryGreen = Integer.parseInt(Integer.toBinaryString(green));
                 int binaryBlue = Integer.parseInt(Integer.toBinaryString(blue));
 
-                /**
-                System.out.println("rojo: " + red);
-                System.out.println("en binario es: " + binaryRed);
-                System.out.println("verde: " + green);
-                System.out.println("en binario es: " + binaryRed);
-                System.out.println("azul: " + blue);
-                System.out.println("en binario es: " + binaryRed);
-                 */
-
-
-                //convierte el input del usuario a un array de bytes por cada caracter
-                String inputWord = "hola#";
-                byte[] bytes = inputWord.getBytes();
-
-                String[] inputWordBytesArray = new String[inputWord.length()];
-                int wordToByteIterator = 0;
-                for (int iterator : bytes) {
-                    inputWordBytesArray[wordToByteIterator] = Integer.toBinaryString(iterator);
-                    wordToByteIterator++;
-                }
-
-                int letterBytePosition = 0;
-                int charPosition = 0;
-                int letterBytesLenght = 0;
-
                 int[] colors = {binaryRed, binaryGreen, binaryBlue};
                 for (int i = 0; i < 3; i++) {
                     String[] letterBytes = inputWordBytesArray[letterBytePosition].split("");
                     letterBytesLenght = letterBytes.length;
+                    if (charPosition > letterBytesLenght-1){
+                        charPosition = 0;
+                        if (letterBytePosition != inputWord.length()-1){
+                            letterBytePosition++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
                     if ((colors[i] % 10) != Integer.parseInt(letterBytes[charPosition])) {
                         if ((colors[i] % 10) == 1) {
                             colors[i]--;
@@ -66,42 +69,16 @@ public class Main {
                             colors[i]++;
                         }
                     }
-                    charPosition++;
+                        charPosition++;
                 }
                 int redBinaryToDecimal = Integer.parseInt(String.valueOf(colors[0]), 2);
                 int greenBinaryToDecimal = Integer.parseInt(String.valueOf(colors[1]), 2);
                 int blueBinaryToDecimal = Integer.parseInt(String.valueOf(colors[2]), 2);
                 color = new Color(redBinaryToDecimal, greenBinaryToDecimal, blueBinaryToDecimal);
                 image.setRGB(x, y, color.getRGB());
-
-
-                if (charPosition > letterBytesLenght) {
-                    charPosition = 0;
-                    letterBytePosition++;
-                    //change position of the char to operate
-                }
             }
         }
-
-
         /**
-         StringBuffer fullAscii = new StringBuffer();
-         for (int i = 0; i < binaryRedArray.length; i++) {
-         fullAscii.append(binaryRedArray[i]);
-         }
-         int converter = Integer.parseInt(String.valueOf(fullAscii), 2);
-
-
-         System.out.println("en int el resultado es: " +converter);
-
-         */
-
-        /**
-
-         color = new Color(red, green, blue);
-         image.setRGB(x, y, color.getRGB());
-
-         //Creates another png with the previous settings
          file = new File("C:\\Users\\alanb\\Desktop\\modified_image.png");
          ImageIO.write(image, "png", file);
          System.out.println("Doners");
